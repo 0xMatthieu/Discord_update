08c183481f0c1f0f223e09850eb8704eb6ccf6a8
Matt <Matt1607@hotmail.com>
2024-11-02 09:33:33 +0100

    feat: Add server name logging on bot ready event

--- discord_bot.py
+++ discord_bot.py
@@ -6,7 +6,16 @@ load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+SERVER_NAME = os.getenv('DISCORD_SERVER_NAME')
+
 class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}')
+        for guild in self.guilds:
+            if guild.name == SERVER_NAME:
+                print(f'Connected to guild: {guild.name}')
+                for channel in guild.text_channels:
+                    print(f'Channel: {channel.name}')
     async def on_ready(self):
         print(f'Logged in as {self.user}')
         for guild in self.guilds:
